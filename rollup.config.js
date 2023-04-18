47fd86aab31af9e9ae91b73b9d6a6e7a3dc33dbd
Akash <akash.chhetri@techaheadcorp.com>
2023-04-18 14:49:36 +0530

    fixed blank screen issue

--- rollup.config.js
+++ rollup.config.js
@@ -14,7 +14,7 @@ export default {
     babel({
       babelHelpers: 'bundled',
       exclude: 'node_modules/**',
-      presets: ['@babel/preset-env','@babel/preset-react']
+      presets: ['@babel/preset-env',['@babel/preset-react', { runtime: "automatic" }]]
     }),
     resolve(),
     commonjs(),
